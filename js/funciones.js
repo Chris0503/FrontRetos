5ff04288bc11b24c14ed545867e29c59602b041c
Chris0503 <91760743+Chris0503@users.noreply.github.com>
2021-11-01 23:07:41 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,24 @@
+function traerInformacionCategoria(){
+    $.ajax({
+        url:"http://localhost:8080/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    });
+}
+
+function pintarRespuesta (respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.lengt;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>"
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado1").append(myTable);
+}
